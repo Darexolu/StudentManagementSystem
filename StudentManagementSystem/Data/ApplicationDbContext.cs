d4bee9e52407da5fc80ac00069cb9701af218694
SDSD STAFF <SDSD STAFF@DESKTOP-3L41P6T>
2024-02-15 13:06:16 +0100

    Add project files.

---
+++ StudentManagementSystem/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementSystem.Shared.Models;
+using System.Collections.Generic;
+
+namespace StudentManagementSystem.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+    
+}
---
+++ StudentsManagementShared/StudentRepository/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentManagementSystem.Shared.Models;
+
+namespace StudentsManagementShared.StudentRepository
+{
+    public interface IStudentRepository
+    {
+        Task<Student> AddStudentAsync(Student student);
+        Task<Student> UpdateStudentAsync(Student student);
+        Task<Student> DeleteStudentAsync(Student student);
+        Task<List<Student>> GetAllStudentsAsync(Student student);
+    }
+}
